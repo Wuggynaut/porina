16c193c30eaec7c9645a3b63da0c34d0dfcc6726
Wuggy <ari-matti.toivonen@myy.haaga-helia.fi>
2026-04-15 15:39:48 +0300

    started on recipe list with headers

--- app/(tabs)/recipes/index.tsx
+++ app/(tabs)/recipes/index.tsx
@@ -1,17 +1,42 @@
-import {FlatList, Pressable, Text, View} from "react-native";
+import {SectionList, Pressable, Text, View} from "react-native";
 import {Link} from "expo-router";
 import {recipes} from "../../../src/data";
+import {useMemo} from "react";
+import {Recipe} from "../../../src/types/brew";
 
 export default function Recipes() {
+    const sections = useMemo(() => {
+        const grouped = recipes.reduce<Record<string, Recipe[]>>((acc, recipe) => {
+            (acc[recipe.method] ??= []).push(recipe);
+            return acc;
+        }, {});
 
+        return Object.entries(grouped).map(([method, data]) => ({
+            title: method,
+            data,
+        }));
+    }, [recipes]);
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Recipes</Text>
-            <FlatList data={recipes} renderItem={({item}) =>
-                <Link href={`/recipes/${item.id}`} asChild>
-                    <Pressable><Text>{item.name}</Text></Pressable>
-                </Link>
-            } />
+
+            <SectionList
+                sections={sections}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <View>
+                        <Link href={`/recipes/${item.id}`}>
+                            <Pressable>
+                                <Text>{item.name}</Text>
+                            </Pressable>
+                        </Link>
+                    </View>
+                )}
+                renderSectionHeader={({section: {title} }) => (
+                    <Text>{title}</Text>
+                )}
+                stickySectionHeadersEnabled={false}
+            />
         </View>
     )
 }
