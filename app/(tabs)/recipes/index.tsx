3491942cfb16fe089338dcc0b28ee8396c2ba670
Wuggy <ari-matti.toivonen@myy.haaga-helia.fi>
2026-04-15 15:10:27 +0300

    readme and recipe list

--- app/(tabs)/recipes/index.tsx
+++ app/(tabs)/recipes/index.tsx
@@ -1,13 +1,17 @@
-import {Pressable, Text, View} from "react-native";
+import {FlatList, Pressable, Text, View} from "react-native";
 import {Link} from "expo-router";
+import {recipes} from "../../../src/data";
 
 export default function Recipes() {
+
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Recipes</Text>
-            <Link href="/recipes/hoffman-v60" asChild>
-                <Pressable><Text>V60 recipe</Text></Pressable>
-            </Link>
+            <FlatList data={recipes} renderItem={({item}) =>
+                <Link href={`/recipes/${item.id}`} asChild>
+                    <Pressable><Text>{item.name}</Text></Pressable>
+                </Link>
+            } />
         </View>
     )
 }
